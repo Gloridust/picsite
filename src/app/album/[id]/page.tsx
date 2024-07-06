68dae05295671c43ba6b603f28f0c32df0ddb07e
Gloridust <Gloridust@gmail.com>
2024-07-07 02:59:12 +0800

    Update page.tsx

---
+++ src/app/album/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from 'next/navigation'
+import ImageGrid from '@/app/components/ImageGrid'
+import { getAlbumById } from '@/app/utils/albums'
+
+export default function Album({ params }: { params: { id: string } }) {
+  const album = getAlbumById(params.id)
+
+  if (!album) {
+    notFound()
+  }
+
+  return (
+    <div>
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold">{album.name}</h1>
+        <p className="text-gray-600">{album.date}</p>
+        <p>{album.description}</p>
+      </div>
+      <ImageGrid images={album.images} />
+    </div>
+  )
+}
