6f52a21084c6e44826b62d994f8bcc2ce48e7eb9
Gloridust <Gloridust@gmail.com>
2024-07-07 03:10:51 +0800

    Update AlbumGrid.tsx

---
+++ src/app/components/AlbumGrid.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Album } from '@/types/album'
+import AlbumCard from './AlbumCard'
+
+interface AlbumGridProps {
+  albums: Album[]
+}
+
+const AlbumGrid: React.FC<AlbumGridProps> = ({ albums }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {albums.map((album) => (
+        <AlbumCard key={album.id} album={album} />
+      ))}
+    </div>
+  )
+}
+
+export default AlbumGrid
