074492fcf0e69f3d22ad93ea16fbf55da9705c71
Gloridust <Gloridust@gmail.com>
2024-07-07 02:59:14 +0800

    Update Footer.tsx

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-[#f3f3f3] p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} PicSite. All rights reserved.</p>
+      </footer>
+    )
+  }
+  
+  export default Footer
