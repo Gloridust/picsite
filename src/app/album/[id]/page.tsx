c46cc0c7d5d5b833f631522cb43d7e0e4361df8d
Gloridust <Gloridust@gmail.com>
2024-07-07 03:28:46 +0800

    Update page.tsx

--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -10,7 +10,7 @@ export default function Album({ params }: { params: { id: string } }) {
   }
 
   return (
-    <div>
+    <div className="container mx-auto px-4">
       <div className="mb-8">
         <h1 className="text-3xl font-bold">{album.name}</h1>
         <p className="text-gray-600">{album.date}</p>
