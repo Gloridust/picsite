0bd83a23e30e77043a8beb33c70e39e5591dbcf4
Gloridust <Gloridust@gmail.com>
2024-07-09 19:15:48 +0800

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            img: {
+              maxWidth: '100%',
+              height: 'auto',
+              borderRadius: theme('borderRadius.lg'),
+              margin: '1rem auto',
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
