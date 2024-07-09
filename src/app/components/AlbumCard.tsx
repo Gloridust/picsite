fd1042e50b8f899058e86b45bdc9b6bb1cc1eeea
Gloridust <Gloridust@gmail.com>
2024-07-09 19:31:59 +0800

    add: img loading

--- src/app/components/AlbumCard.tsx
+++ src/app/components/AlbumCard.tsx
@@ -14,8 +14,11 @@ const AlbumCard: React.FC<AlbumCardProps> = ({ album }) => {
           <Image
             src={album.coverImage}
             alt={album.name}
-            layout="fill"
-            objectFit="cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="object-cover"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           />
         </div>
         <div className="p-4">
@@ -27,5 +30,25 @@ const AlbumCard: React.FC<AlbumCardProps> = ({ album }) => {
     </Link>
   )
 }
+
+// 辅助函数：生成 SVG
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 export default AlbumCard
