e0b74681aa00a2033284ecb68ebe13ec898d06fc
Gloridust <Gloridust@gmail.com>
2024-07-09 15:16:01 +0800

    add:about.md

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,16 @@
+import fs from 'fs'
+import path from 'path'
 import ReactMarkdown from 'react-markdown'
 
-const aboutContent = `
-# 关于 PicSite
-
-PicSite 是一个简单而强大的在线图片展示网站。
-
-## 主要功能
-
-- 按相册分类展示图片
-- 搜索相册功能
-- 响应式设计，支持移动端和桌面端
-`
-
 export default function About() {
+  const aboutPath = path.join(process.cwd(), 'src/content/about.md')
+  const aboutContent = fs.readFileSync(aboutPath, 'utf8')
+
   return (
-    <div className="prose">
-      <ReactMarkdown>{aboutContent}</ReactMarkdown>
+    <div className="container mx-auto px-4 py-8">
+      <div className="prose max-w-none">
+        <ReactMarkdown>{aboutContent}</ReactMarkdown>
+      </div>
     </div>
   )
 }
