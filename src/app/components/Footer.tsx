fef54c0d568796f3932404c9e2d0ac2851dadb07
Gloridust <Gloridust@gmail.com>
2024-07-11 00:03:32 +0800

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-[#f3f3f3] p-4 text-center">
-        <p>&copy; {new Date().getFullYear()} PicSite. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} <a href="https://github.com/Gloridust/picsite">PicSite on Github</a>. All rights reserved.</p>
       </footer>
     )
   }
